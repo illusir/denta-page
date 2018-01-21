dbe0c71949a6f934f1825b589b41906be6fd2b09
Illusir <taom.fox@gmail.com>
2018-01-21 23:46:58 +0400

    commit 210118-1

--- js/slick-init.js
+++ js/slick-init.js
@@ -3,12 +3,18 @@ $(".slider-firstscreen").slick({
     appendArrows: $('.slider-arrow'),
     prevArrow: $('.slider-arrow--left'),
     nextArrow: $('.slider-arrow--right'),
-
 });
+
 $(".slider-about").slick({
 	dots: true,
     appendArrows: $('.about-arrow'),
     prevArrow: $('.about-arrow--left'),
     nextArrow: $('.about-arrow--right'),
+});
 
+$(".slider-feedback").slick({
+	dots: false,
+    appendArrows: $('.feedback-arrow'),
+    prevArrow: $('.feedback-arrow--left'),
+    nextArrow: $('.feedback-arrow--right'),
 });
