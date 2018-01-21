4862d000bcc201e8cae0496d509861cad39b9dfb
Illusir <taom.fox@gmail.com>
2018-01-21 17:44:58 +0400

    commit 210118-1

---
+++ js/slick-init.js
@@ -0,0 +1,14 @@
+$(".slider-firstscreen").slick({
+	dots: true,
+    appendArrows: $('.slider-arrow'),
+    prevArrow: $('.slider-arrow--left'),
+    nextArrow: $('.slider-arrow--right'),
+
+});
+$(".slider-about").slick({
+	dots: true,
+    appendArrows: $('.about-arrow'),
+    prevArrow: $('.about-arrow--left'),
+    nextArrow: $('.about-arrow--right'),
+
+});
